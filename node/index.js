218b05e311d3a9a2d12cb7053676ee3be499daa8
thi-costa <41833533+thi-costa@users.noreply.github.com>
2024-01-23 22:05:08 -0300

    feat: add nginx and connect node app to it

--- node/index.js
+++ node/index.js
@@ -3,8 +3,20 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+// Permitir apenas o host que passa pelo nginx
+    // Bloqueia o acesso direto 'http://localhost:81'
+const allowedHost = 'localhost';
+
+app.use((req, res, next) => {
+    const requestHost = req.get('host');
+    if ( requestHost !== allowedHost ){
+        return res.status(403).send('Forbidden - Access not allowed');
+    }
+    next();
+})
+
 app.get('/', (req, res) => {
-    res.send('<h1>Fullcycle</h1>');
+    res.send('<h1>Full Cycle Rocks!</h1>');
 })
 
 app.listen(port, () => {
